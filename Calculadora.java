f1e28cdfca7392df7da973862172fc91d9820763
Marlon Luis Aponte Sierra <marlonaponte001@gmail.com>
2020-04-22 20:52:57 -0500

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Marlon
+ */
+public class Calculadora {
+
+    
+    
+    public static int  sumar (int a, int b) {
+        return a+b;    
+    }
+    
+    public static int  resta (int a, int b) {
+        return a-b;    
+    }
+    
+    public static int  multiplicacion (int a, int b) {
+        return a*b;    
+    }
+    
+    public static float  division (float a, float b) {
+        float div=0;
+        try {
+            div= a/b;
+        } catch (ArithmeticException e) {
+            System.out.print("no se puede dividir entre cero");
+        }
+        return div;    
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Suma= "+ sumar(10, 20));
+        System.out.println("Resta= "+resta(10, 20));
+        System.out.println("Multiplicacion= "+ multiplicacion(10, 20));
+        System.out.println("Division= " + division(10, 5));
+        
+    
+
+ 
+
+    }
+
+ 
+
+
+    
+}
